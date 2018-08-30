8b9737172b5401ea1531dec2e3bd2db0ec0145d3
james gover <james.i.gover@gmail.com>
2018-08-30 15:48:18 +0800

    started two files, one for playing around with blob image other contains notes on what must be done

---
+++ test_import.py
@@ -0,0 +1,11 @@
+
+
+'''
+process is:
+1. determine track rate
+2. take a unique frame when possible
+3. create a binary image
+4. seperate binary into labeled seperate objects
+5. for each labeled object, isolate
+6. compare
+'''
