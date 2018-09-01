8c22f3ec848063ad67177f5a8c843bdf4c17ffa6
james gover <james.i.gover@gmail.com>
2018-09-02 00:22:30 +0800

    WARNING: accesses webcam and saves 5 frames. further testing of video capture into image production

--- Video_to_img.py
+++ Video_to_img.py
@@ -1,28 +1,34 @@
 import numpy as np
 import cv2
 import os
+FOLDER_NAME = 'frames'
 
-def create_dir():
+
+def create_dir(folder=FOLDER_NAME):
     try:
-        if not os.path.exists('frames'):
-            os.makedirs('frames')
+        if not os.path.exists(folder):
+            os.makedirs(folder)
     except OSError:
         print('Error: Creating directory of data')
 
-cap = cv2.VideoCapture(0)
 
-while(True):
+
+inc = 0
+#create_dir()
+cap = cv2.VideoCapture(0)
+while inc < 5:
     # Capture frame-by-frame
     ret, frame = cap.read()
 
-    # Our operations on the frame come here
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-
     # Display the resulting frame
-    cv2.imshow('frame',gray)
+    cv2.imshow('frame.', frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
+    frame_name = "./" + FOLDER_NAME + "/pot_frame" + str(inc) + ".jpg"
+    cv2.imwrite(frame_name, frame)
+    inc += 1
 
 # When everything done, release the capture
 cap.release()
 cv2.destroyAllWindows()
+
