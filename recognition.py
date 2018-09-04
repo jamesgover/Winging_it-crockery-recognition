1df7f903197f7c7a5f3ba4d9ef4f2029b9162684
james gover <james.i.gover@gmail.com>
2018-09-04 22:37:37 +0800

    added some stuff to save and load the data. and some processing of recorded images

---
+++ recognition.py
@@ -0,0 +1,12 @@
+# need to classify three seperate tasks
+
+# #####_____Crockery_____#####
+# Bowls, Plates, Trays, Cups, bread tray, other?
+
+
+# #####_____Cutlery_____#####
+# Forks, Knifes, Spoons, Teaspoons
+
+
+# #####_____Foils_____#####
+# colour separation (camera easy tag identification)
